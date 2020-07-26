1b43458fd39d7161c3d51a24a83380f578d1c78d
elikrm <elnazkarimpoor85@gmail.com>
2020-07-26 20:12:30 +1000

    uncomment some part of the code

--- Loop_exercise-solution.py
+++ Loop_exercise-solution.py
@@ -1,38 +1,38 @@
-# #Q1) Continuously ask the user to enter a number until they provide a blank input. 
-# # Output the sum of all the numbers.
+#Q1) Continuously ask the user to enter a number until they provide a blank input. 
+# Output the sum of all the numbers.
 
-# # isnumeric() Returns True if all characters in the string are numeric
-# # isspace()	Returns True if all characters in the string are whitespaces
-# yournumber = input("Enter a number: ")
-# totalnumber = 0
-# while((len(yournumber)!=0) and (yournumber.isnumeric()) and not(yournumber.isspace())):
-# # while(len(yournumber)!=0):
-#     # print(yournumber.isnumeric())
-#     # print(not yournumber.isspace())
-#     totalnumber = totalnumber + int(yournumber)
-#     yournumber = input("Enter a number: ")
-# print(totalnumber)
-# #Q2) Use a for loop to format and print the following list:
-# mailing_list = [
-# ["Roary", "roary@moth.catchers"],
-# ["Remus", "remus@kapers.dog"],
-# ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
-# ["Biscuit", "biscuit@whippies.park"],
-# ["Rory", "rory@whippies.park"],
-# ]
-# for item in mailing_list:
-#     print("{} : {}".format(item[0],item[1]))
-# nameList = []
+# isnumeric() Returns True if all characters in the string are numeric
+# isspace()	Returns True if all characters in the string are whitespaces
+yournumber = input("Enter a number: ")
+totalnumber = 0
+while((len(yournumber)!=0) and (yournumber.isnumeric()) and not(yournumber.isspace())):
+# while(len(yournumber)!=0):
+    # print(yournumber.isnumeric())
+    # print(not yournumber.isspace())
+    totalnumber = totalnumber + int(yournumber)
+    yournumber = input("Enter a number: ")
+print(totalnumber)
+#Q2) Use a for loop to format and print the following list:
+mailing_list = [
+["Roary", "roary@moth.catchers"],
+["Remus", "remus@kapers.dog"],
+["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+["Biscuit", "biscuit@whippies.park"],
+["Rory", "rory@whippies.park"],
+]
+for item in mailing_list:
+    print("{} : {}".format(item[0],item[1]))
+nameList = []
 
-# # Q3) Use a while loop to ask the user for three names and append them to a list, 
-# # then use a for loop to print the list.
-# counter = 3
-# nameList = []
-# while(counter>0):
-#     enterName =input("enter a name: ")
-#     nameList.append(enterName)
-#     counter -= 1
-# print(nameList)
+# Q3) Use a while loop to ask the user for three names and append them to a list, 
+# then use a for loop to print the list.
+counter = 3
+nameList = []
+while(counter>0):
+    enterName =input("enter a name: ")
+    nameList.append(enterName)
+    counter -= 1
+print(nameList)
 
 #Q4) Ask the user how many units of each item they bought, 
 # then output the corresponding receipt.
